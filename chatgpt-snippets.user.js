c2f24566cb41948eb3c6acfa97330e0be24a155e
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2023-05-25 03:45:19 +0800

    Name popup + fix field selector

--- chatgpt-snippets.user.js
+++ chatgpt-snippets.user.js
@@ -13,9 +13,14 @@
 
 let field = null;
 let snippetsWrapper = null;
+let namePopup = null;
+let nameInput = null;
+let saveSnippetBtn = null;
+let saveSnippetBtnDisabled = true;
+let shouldPreventNameChanging = false
 
 function init() {
-  const foundField = document.querySelector('textarea[placeholder="Send a message."]')
+  const foundField = document.getElementById('prompt-textarea')
 
   if (!foundField || foundField === field) {
     return
@@ -30,6 +35,7 @@ function init() {
 
   injectSnippets();
   injectSaveSnippetBtn();
+  injectNamePopup();
 }
 
 setInterval(init, 1000)
@@ -112,6 +118,52 @@ document.head.append(createLayoutFromString(`
       background: rgba(255, 255, 255, .1);
       transition: background .2s;
     }
+
+    .mf-name-popup {
+      position: absolute;
+      bottom: 2px;
+      right: 82px;
+      width: 200px;
+      height: 44px;
+      transition: opacity .15s;
+
+      opacity: 0;
+      pointer-events: none;
+    }
+
+    .mf-name-popup__title {
+      position: absolute;
+      left: 8px;
+      font-size: 11px;
+      color: #000;
+      pointer-events: none;
+    }
+
+    .mf-name-popup__input {
+      width: 100%;
+      padding: 10px;
+      height: 100%;
+      font-size: 14px;
+      color: #000;
+      border: 0;
+      border-radius: 6px;
+    }
+
+    .mf-name-popup::after {
+      content: "";
+      position: absolute;
+      width: 0;
+      height: 0;
+      margin-left: 0;
+      bottom: 0;
+      top: calc(50% - 5px);
+      right: -5px;
+      box-sizing: border-box;
+      border: 5px solid #fff;
+      border-color: #fff #fff transparent transparent;
+      transform-origin: 5px 5px;
+      transform: rotate(45deg);
+    }
   </style>
 `))
 
@@ -125,9 +177,12 @@ function injectSnippets() {
 
 function injectSnippetBtn(snippet) {
   const snippetBtn = createLayoutFromString(`
-    <div class="mf-snippet">
+    <div
+      class="mf-snippet"
+      data-id="${snippet.id}"
+    >
       <div class="mf-snippet__text">
-        ${snippet}
+        ${snippet.name}
       </div>
 
       <div class="mf-snippet__remove">
@@ -141,7 +196,7 @@ function injectSnippetBtn(snippet) {
   snippetBtn.querySelector('.mf-snippet__text').onclick = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    field.value = snippet;
+    field.value = snippet.content;
     field.focus();
     field.dispatchEvent(new Event('input', { bubbles: true })); // trigger field resize
   }
@@ -149,20 +204,17 @@ function injectSnippetBtn(snippet) {
   snippetBtn.querySelector('.mf-snippet__remove').onclick = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    removeSnippet(snippetBtn, snippet);
+
+    const savedSnippets = getSavedSnippets();
+    const snippetsWithoutRemoved = savedSnippets.filter((s) => s.id !== snippet.id);
+    saveSnippets(snippetsWithoutRemoved);
+
+    snippetBtn.remove();
   }
 }
 
-function removeSnippet(snippetBtn, snippet) {
-  snippetBtn.remove();
-  const savedSnippets = getSavedSnippets();
-  const removingSnippetIndex = savedSnippets.indexOf(snippet);
-  savedSnippets.splice(removingSnippetIndex, 1);
-  saveSnippets(savedSnippets);
-}
-
 function injectSaveSnippetBtn() {
-  const saveSnippetBtn = createLayoutFromString(`
+  saveSnippetBtn = createLayoutFromString(`
     <button class="mf-save-snippet-btn">
       <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M16 8.98987V20.3499C16 21.7999 14.96 22.4099 13.69 21.7099L9.76001 19.5199C9.34001 19.2899 8.65999 19.2899 8.23999 19.5199L4.31 21.7099C3.04 22.4099 2 21.7999 2 20.3499V8.98987C2 7.27987 3.39999 5.87988 5.10999 5.87988H12.89C14.6 5.87988 16 7.27987 16 8.98987Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -178,13 +230,109 @@ function injectSaveSnippetBtn() {
   saveSnippetBtn.onclick = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    saveSnippet()
+
+    if (saveSnippetBtnDisabled) {
+      return
+    }
+
+    addSnippet()
   }
+}
+
+function injectNamePopup() {
+  namePopup = createLayoutFromString(`
+    <div class="mf-name-popup">
+      <div class="mf-name-popup__title">
+        You can set shorter name
+      </div>
+
+      <input
+        type="text"
+        class="mf-name-popup__input"
+      />
+    </div>
+  `)
+
+
+  field.parentElement.append(namePopup)
+
+  let isFieldFocused = false
+
+  const [runHideDebounce, cancelHideDebounce] = debounce(() => {
+    if (isFieldFocused) {
+      return;
+    }
+
+    namePopup.style.opacity = 0;
+    namePopup.style.pointerEvents = 'none';
+  }, 300)
+
+  const show = () => {
+    cancelHideDebounce()
+    namePopup.style.opacity = 1;
+    namePopup.style.pointerEvents = 'auto';
+  }
+
+  saveSnippetBtn.onmouseover = show
+  saveSnippetBtn.onmouseleave = runHideDebounce
+  namePopup.onmouseover = show
+  namePopup.onmouseleave = runHideDebounce
+
+  nameInput = namePopup.querySelector('.mf-name-popup__input')
+
+  nameInput.onfocus = () => {
+    isFieldFocused = true;
+    show()
+  };
+
+  nameInput.onblur = () => {
+    isFieldFocused = false;
+    runHideDebounce()
+  };
+
+  updateSaveSnippetBtnDisabledState()
+
+  nameInput.oninput = (e) => {
+    shouldPreventNameChanging = !!e.target.value.length
+    updateSaveSnippetBtnDisabledState()
+  };
+
+  field.addEventListener('input', () => {
+    if (shouldPreventNameChanging) {
+      return
+    }
+
+    nameInput.value = field.value;
+    updateSaveSnippetBtnDisabledState()
+  })
+}
+
+const updateSaveSnippetBtnDisabledState = () => {
+  saveSnippetBtnDisabled = !nameInput.value.length
 }
 
 function getSavedSnippets() {
   try {
-    return JSON.parse(localStorage.getItem('chatgpt_snippets.snippets')) || []
+    const savedSnippetsVersionIndex = localStorage.getItem('chatgpt_snippets.version') || 0
+    const savedSnippets = JSON.parse(localStorage.getItem('chatgpt_snippets.snippets')) || []
+
+    const migrations = [
+      () => savedSnippets.map((content) => ({
+        id: Date.now(),
+        name: content,
+        content
+      }))
+    ]
+    const requiredMigrations = migrations.slice(savedSnippetsVersionIndex)
+
+    const migratedSnippets = requiredMigrations.reduce((acc, migration) => {
+      return migration(acc)
+    }, savedSnippets)
+
+    localStorage.setItem('chatgpt_snippets.version', migrations.length)
+    localStorage.setItem('chatgpt_snippets.snippets', JSON.stringify(migratedSnippets))
+
+    return migratedSnippets
   } catch {
     return [];
   }
@@ -197,8 +345,16 @@ function saveSnippets(savingSnippets) {
   );
 }
 
-function saveSnippet() {
-  const newSnippet = field.value;
+function addSnippet() {
+  const newSnippet = {
+    id: Date.now(),
+    name: nameInput.value,
+    content: field.value
+  }
+
+  nameInput.value = '';
+  updateSaveSnippetBtnDisabledState()
+  shouldPreventNameChanging = false;
 
   const savedSnippets = getSavedSnippets();
   savedSnippets.push(newSnippet);
@@ -213,4 +369,20 @@ function createLayoutFromString(template) {
   div.innerHTML = template
 
   return div.children[0]
+}
+
+function debounce(f, ms) {
+  let timeoutId;
+
+  return [
+    (...args) => {
+      clearTimeout(timeoutId);
+
+      timeoutId = setTimeout(
+        () => f(...args),
+        ms
+      );
+    },
+    () => clearTimeout(timeoutId)
+  ];
 }
